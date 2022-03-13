6fd8c2a6bfdeb28ac39a9f35e2e40a98d89d57de
nextGenGamer2020 <85717794+nextGenGamer2020@users.noreply.github.com>
2022-03-13 18:41:43 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+noseX = 0
+noseY = 0
+rightWrist = 0
+leftWrist = 0
+difference = 0
+
+function setup(){
+    canvas = createCanvas(550,450)
+    canvas.position(580,100)
+
+    video = createCapture(VIDEO)
+    video.size(550,500)
+
+    poseNet = ml5.poseNet(video, modelLoaded)
+
+    poseNet.on('pose', gotPoses)
+}
+function draw(){
+    document.getElementById("square_side").innerHTML = "Width and height of a square will be = "+ difference+"px"
+    background("dodgerblue")
+    fill("#fc0377")
+    stroke("#fcb503")
+    square(noseX,noseY, difference)
+    
+}
+
+
+function modelLoaded(){
+    console.log("Model is initialized")
+}
+
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results)
+    noseX = results[0].pose.nose.x
+    noseY = results[0].pose.nose.y
+    console.log("noseX = "+ noseX + "noseY"+noseY)
+
+    rightWrist = results[0].pose.rightWrist.x
+    leftWrist = results[0].pose.leftWrist.x
+    differnece = floor(leftWrist - rightWrist)
+    console.log("rightWrist = "+ rightWrist + "leftWrist"+leftWrist+"difference"+difference)
+    }
+
+}
+
